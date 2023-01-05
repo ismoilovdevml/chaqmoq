48713eb06bad83dc706e14cc82d7a57be11ca683
ismoilovdevml <ismoilovdevarchlinux@gmail.com>
2023-01-05 19:18:14 +0500

    cross-platform syscall

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 // use std::arch::asm;
 
 // fn main() {
-//     let message = String::from("Chaqmoqdan salom");
+//     let message = String::from("Chaqmoqdan salom, linux syscall");
 //     syscall(message)
 // }
 
@@ -30,11 +30,11 @@
 // use std::arch::asm;
 
 // fn main() {
-//     let message = String::from("Chaqmoqdan salom");
+//     let message = String::from("Chaqmoqdan salom, macos syscall");
 //     syscall(message)
 // }
 
-// #[cfg(target_os ="linux")]
+// #[cfg(target_os = "linux")]
 // #[inline(never)]
 
 // fn syscall(message: String){
@@ -51,13 +51,12 @@
 //         );
 //     }
 // }
-
-// Linux and MacOS write syscall
+// Linux and MacOS syscall
 
 // use std::io;
 
 // fn main() {
-//     let sys_message = String::from("Chaqmoqdan salom syscall");
+//     let sys_message = String::from("Chaqmoqdan salom, Linux and MacOS syscall");
 //     syscall(sys_message).unwrap();
 // }
 
@@ -83,15 +82,83 @@
 
 // Windows write syscall
 
+// use std::io;
+
+// fn main() {
+//     let sys_message = String::from("Chaqmoqdan salom, windows syscall");
+//     syscall(sys_message).unwrap();
+// }
+
+// #[cfg(target_os = "windows")]
+// #[link(name = "kernel32")]
+
+// extern "stdcall" {
+//     fn GetStdHandle(nStdHandle: i32) -> i32;
+
+//     fn WriteConsoleW(
+//         hConsoleOutput: i32,
+//         lpBuffer: *const u16,
+//         numberOfCharsToWrite: u32,
+//         lpReserved: *const std::ffi::c_void,
+//     ) -> i32;
+// }
+
+// #[cfg(target_os = "windows")]
+
+// fn syscall(message: String) -> io::Result<()> {
+//     let msg: Vec<u16> = message.encode_utf16().collect();
+//     let msg_ptr = msg.as_ptr();
+//     let len = msg.len()as u32;
+    
+//     let mut output: u32 = 0;
+//         let handle = unsafe { GetStdHandle(-11) };
+//         if handle == -1 {
+//             return Err(io::Error::last_os_error())
+//         }
+
+//         let res = unsafe {
+//             WriteConsoleW(handle, msg_ptr, len, &mut output, std::ptr::null())
+//         };
+
+//         if res == 0 {
+//             return Err(io::Error::last_os_error());
+//         }
+//     assert_eq!(output as usize, len);
+//     Ok(())
+// }
+
+// cross-platform syscall (linux, macos, windows)
+
 use std::io;
 
 fn main() {
-    let sys_message = String::from("Chaqmoqdan salom, windows syscall");
+    let sys_message = String::from("Chaqmoqdan salom, cross-platform syscall ");
     syscall(sys_message).unwrap();
 }
 
+#[cfg(not(target_os = "windows"))]
+#[link(name = "c")]
+
+
+extern "C" {
+    fn write(fd: u32, buf: *const u8, count: usize) -> i32;
+}
+
+#[cfg(not(target_os = "windows"))]
+
+fn syscall(message: String) -> io::Result<()> {
+    let msg_ptr = message.as_ptr();
+    let len = message.len();
+    let res = unsafe { write(1, msg_ptr, len)};
+
+    if res == -1 {
+        return Err(io::Error::last_os_error());
+    }
+    Ok(())
+}
+
 #[cfg(target_os = "windows")]
-#[link(name "kernel32")]
+#[link(name = "kernel32")]
 
 extern "stdcall" {
     fn GetStdHandle(nStdHandle: i32) -> i32;
@@ -99,31 +166,30 @@ extern "stdcall" {
     fn WriteConsoleW(
         hConsoleOutput: i32,
         lpBuffer: *const u16,
-        numberOfCharsToWrite: u32,
+        numberOfCharsWritten: *mut u32,
         lpReserved: *const std::ffi::c_void,
     ) -> i32;
 }
 
 #[cfg(target_os = "windows")]
-
 fn syscall(message: String) -> io::Result<()> {
+
     let msg: Vec<u16> = message.encode_utf16().collect();
     let msg_ptr = msg.as_ptr();
-    let len = msg.len()as u32;
-    
+    let len = msg.len() as u32;
+
     let mut output: u32 = 0;
-        let handle = unsafe { GetStdHandle(-11) };
-        if handle == -1 {
-            return Err(io::Error::last_os_error())
-        }
+    let handle = unsafe { GetStdHandle(-11) };
+    if handle == -1 {
+        return Err(io::Error::last_os_error());
+    }
 
-        let res = unsafe {
-            WriteConsoleW(handle, msg_ptr, len, &mut output, std::ptr::null())
-        };
+    let res = unsafe { WriteConsoleW(handle, msg_ptr, len, &mut output, std::ptr::null())};
 
-        if res == 0 {
-            return Err(io::Error::last_os_error());
-        }
-    assert_eq!(output as usize, len);
+    if res == 0 {
+        return Err(io::Error::last_os_error());
+    }
+
+    assert_eq!(output, len);
     Ok(())
 }
