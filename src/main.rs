7f3fa1706b4eafc77ad328c24a30b215a0d115c8
ismoilovdevml <ismoilovdevarchlinux@gmail.com>
2023-01-05 13:14:39 +0500

    [Chaqmoq]

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+// Linux a write syscall
+
+use::std::arch::asm;
+
+fn main() {
+    let message = String::from("Chaqmodan salom");
+    syscall(message)
+}
+
+#[cfg(target_os = "linux")]
+#[inline(never)]
+
+fn syscall(message: String) {
+    let msg_ptr = message.as_ptr();
+    let len = message.len();
+    unsafe {
+        asm!(
+            "mov rax, 1",
+            "mov rdi, 1",
+            "syscall",
+            in("rsi") msg_ptr,
+            in("rdx") len,
+            out("rax") _, out("rdi") _, lateout("rsi") _, lateout("rdx") _
+        );
+    }
+}
