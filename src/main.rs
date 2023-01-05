093913b102407acd592a753b050fd0b728345f11
ismoilovdevml <ismoilovdevarchlinux@gmail.com>
2023-01-05 13:32:41 +0500

    macos syscall

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,48 @@
 // Linux a write syscall
 
+// use::std::arch::asm;
+
+// fn main() {
+//     let message = String::from("Chaqmoqdan salom");
+//     syscall(message)
+// }
+
+// #[cfg(target_os = "linux")]
+// #[inline(never)]
+
+// fn syscall(message: String) {
+//     let msg_ptr = message.as_ptr();
+//     let len = message.len();
+//     unsafe {
+//         asm!(
+//             "mov rax, 1",
+//             "mov rdi, 1",
+//             "syscall",
+//             in("rsi") msg_ptr,
+//             in("rdx") len,
+//             out("rax") _, out("rdi") _, lateout("rsi") _, lateout("rdx") _
+//         );
+//     }
+// }
+
+// MacOS a write syscall
+
 use::std::arch::asm;
 
 fn main() {
-    let message = String::from("Chaqmodan salom");
+    let message = String::from("Chaqmoqdan salom");
     syscall(message)
 }
 
-#[cfg(target_os = "linux")]
+#[cfg(target_os ="linux")]
 #[inline(never)]
 
-fn syscall(message: String) {
+fn syscall(message: String){
     let msg_ptr = message.as_ptr();
     let len = message.len();
-    unsafe {
+    unsafe{
         asm!(
-            "mov rax, 1",
+            "mov rax, 0x2000004",
             "mov rdi, 1",
             "syscall",
             in("rsi") msg_ptr,
