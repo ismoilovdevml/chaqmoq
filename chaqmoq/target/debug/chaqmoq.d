865168ae47da34a25aecfb1a967f274eb00976da
ismoilovdevml <ismoilovdevarchlinux@gmail.com>
2023-01-12 22:38:08 +0500

    [NEW new chamoqjs

---
+++ chaqmoq/target/debug/chaqmoq.d
@@ -0,0 +1,1 @@
+/home/ismoilovdev/Desktop/Project/chaqmoq/chaqmoq/target/debug/chaqmoq: /home/ismoilovdev/Desktop/Project/chaqmoq/chaqmoq/src/main.rs
---
+++ chaqmoq/target/debug/deps/minimio-87866510ed985379.d
@@ -0,0 +1,8 @@
+/home/ismoilovdev/Desktop/Project/chaqmoq/chaqmoq/target/debug/deps/minimio-87866510ed985379.rmeta: /home/ismoilovdev/.cargo/git/checkouts/examples-minimio-4233db11895ccf6c/f94e2fb/src/lib.rs /home/ismoilovdev/.cargo/git/checkouts/examples-minimio-4233db11895ccf6c/f94e2fb/src/linux.rs
+
+/home/ismoilovdev/Desktop/Project/chaqmoq/chaqmoq/target/debug/deps/libminimio-87866510ed985379.rlib: /home/ismoilovdev/.cargo/git/checkouts/examples-minimio-4233db11895ccf6c/f94e2fb/src/lib.rs /home/ismoilovdev/.cargo/git/checkouts/examples-minimio-4233db11895ccf6c/f94e2fb/src/linux.rs
+
+/home/ismoilovdev/Desktop/Project/chaqmoq/chaqmoq/target/debug/deps/minimio-87866510ed985379.d: /home/ismoilovdev/.cargo/git/checkouts/examples-minimio-4233db11895ccf6c/f94e2fb/src/lib.rs /home/ismoilovdev/.cargo/git/checkouts/examples-minimio-4233db11895ccf6c/f94e2fb/src/linux.rs
+
+/home/ismoilovdev/.cargo/git/checkouts/examples-minimio-4233db11895ccf6c/f94e2fb/src/lib.rs:
+/home/ismoilovdev/.cargo/git/checkouts/examples-minimio-4233db11895ccf6c/f94e2fb/src/linux.rs:
